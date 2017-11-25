efb47e123838904ebac376139d052d6e74cdb29f
Felipe Custódio <felipe.crochi@gmail.com>
2017-11-25 15:30:34 -0200

    script to transform BMP file into bits

---
+++ assets/bmp2bits.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from scipy import misc
+
+if (len(sys.argv) < 2):
+	print("Usage: bmp2bits.py image_file")
+	exit()
+
+path = sys.argv[1]
+image = misc.imread(path, flatten = 1)
+
+for line in image:
+	for bit in line:
+		if (bit == 0):
+			print(1, end='')
+		if (bit == 255):
+			print(0, end='')
+	print('')
+
