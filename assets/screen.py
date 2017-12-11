468ccba1fbaf7612f93d3b64c54a823a0248c778
Felipe Custódio <felipe.crochi@gmail.com>
2017-12-11 15:29:57 -0200

    pipes

--- assets/screen.py
+++ assets/screen.py
@@ -3,13 +3,13 @@ import sys
 import math
 from scipy import misc
 
-if (len(sys.argv) < 2):
+if (len(sys.argv) < 3):
 	print("Usage: image_file")
 	exit()
 
 path = sys.argv[1]
 image = misc.imread(path, flatten = 0)
-
+name = sys.argv[2]
 # color positions
 white = []
 
@@ -29,5 +29,5 @@ for line in image:
 
 index = 0
 for value in white:
-	print("SKY(%d) <= x\"%s\";" % (index,value[2:]))
+	print("%s(%d) <= x\"%s\";" % (name, index,value[2:]))
 	index += 1
